051a97fb70a4e762139e62365fa8e094e1144c47
SYARAH AQILAH <syarahyaacob@gmail.com>
2026-02-12 23:46:09 +0800

    Update email-config.php

--- includes/email-config.php
+++ includes/email-config.php
@@ -11,33 +11,32 @@ require __DIR__ . '/PHPMailer/Exception.php';
 require __DIR__ . '/PHPMailer/PHPMailer.php';
 require __DIR__ . '/PHPMailer/SMTP.php';
 
-// 2. Define the Function
 function sendCredentialsEmail($name, $email, $raw_password, $lecturer_id) {
     
-    // Create a new instance
     $mail = new PHPMailer(true);
 
-   try {
+    try {
         // --- SERVER SETTINGS ---
         $mail->isSMTP();
-        
-        // FIX 1: Force IPv4 by converting the hostname to an IP address immediately
-        $mail->Host       = gethostbyname('smtp.gmail.com'); 
-        
+        $mail->Host       = 'smtp.gmail.com'; 
         $mail->SMTPAuth   = true;                   
         $mail->Username   = 'syarahaqilah@graduate.utm.my'; 
         $mail->Password   = 'hwvm vwiv rmnv iuoj';   
         
-        // FIX 2: Use TLS on Port 587 (Standard for "insecure" IP connections)
+        // Revert to Port 587 (Standard TLS)
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
-        // FIX 3: Disable strict SSL checks (Required because we are using an IP, not a domain)
+        // --- THE MAGIC FIX ---
+        // This forces the connection to use the old IPv4 standard
         $mail->SMTPOptions = array(
             'ssl' => array(
                 'verify_peer'       => false,
                 'verify_peer_name'  => false,
                 'allow_self_signed' => true
+            ),
+            'socket' => array( 
+                'bindto' => '0.0.0.0:0' 
             )
         );
 
@@ -56,17 +55,15 @@ function sendCredentialsEmail($name, $email, $raw_password, $lecturer_id) {
                 <li><strong>Username:</strong> $lecturer_id</li>
                 <li><strong>Password:</strong> $raw_password</li>
             </ul>
-            <p>Please login and change your password.</p>
         ";
 
         $mail->send();
-        return true;
-        
+        return true; 
+
     } catch (Exception $e) {
-        // FIX: Display the error so we can fix it
         echo "<b>MAILER ERROR:</b> " . $mail->ErrorInfo; 
-        // return false; // Keep this commented out until it works
         die(); 
     }
 }
+
 ?>
